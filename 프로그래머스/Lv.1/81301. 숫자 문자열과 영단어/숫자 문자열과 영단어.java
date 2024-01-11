97687d3f1301816aee0eadfa041058659a71fb7e
권석근 <121447330+Broot96@users.noreply.github.com>
2024-01-11 23:02:18 +0900

    [level Lv.1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        
+        StringBuilder sb = new StringBuilder();
+        
+        Map<String, Integer> numberMap = new HashMap<String, Integer>();
+        String[] number =  {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        
+        for(int i = 0; i < 10; i++){
+            numberMap.put(number[i], i);
+        }
+        
+        StringBuilder tmp = new StringBuilder();
+        for(char c : s.toCharArray()){
+            if(c >= '0' && c <= '9'){
+                sb.append(c);
+            }else{
+                tmp.append(c);
+                if(numberMap.containsKey(tmp.toString())){
+                    sb.append(numberMap.get(tmp.toString()));
+                    tmp = new StringBuilder();
+                }
+            }
+        }
+        
+        answer = Integer.parseInt(sb.toString());
+        
+        return answer;
+    }
+}
