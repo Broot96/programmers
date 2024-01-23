a2fc0970477b02a97bbfb0079f1a455ff220e41a
권석근 <121447330+Broot96@users.noreply.github.com>
2024-01-23 20:48:19 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,15 +1,13 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 class Solution {
-    boolean answer = false;
-    public boolean solution(String s) {
-        if(s.length() == 4 ||s.length() == 6){
-            Pattern pt = Pattern.compile("^[0-9]*$");
-            Matcher match = pt.matcher(s);
-
-            return match.find();
-        }
-        return answer;
-    }
+  public boolean solution(String s) {
+      if(s.length() == 4 || s.length() == 6){
+          try{
+              int x = Integer.parseInt(s);
+              return true;
+          } catch(NumberFormatException e){
+              return false;
+          }
+      }
+      else return false;
+  }
 }
